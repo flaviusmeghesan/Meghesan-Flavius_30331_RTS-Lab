bb4e8a2d671d854755428bd22d4bf916454fb6c2
rlkk <flv.meghe@gmail.com>
2024-03-02 22:54:48 +0200

    Laboratory 1 ex 1 complete

---
+++ src/Laboratory1/Exercise1/ComplexNumber.java
@@ -0,0 +1,30 @@
+package Laboratory1.Exercise1;
+
+public class ComplexNumber {
+    double real;
+    double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber add(ComplexNumber other) {
+        return new ComplexNumber(this.real + other.real, this.imaginary + other.imaginary);
+    }
+
+    public ComplexNumber subtract(ComplexNumber other) {
+        return new ComplexNumber(this.real - other.real, this.imaginary - other.imaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber other) {
+        double resultReal = this.real * other.real - this.imaginary * other.imaginary;
+        double resultImaginary = this.real * other.imaginary + this.imaginary * other.real;
+        return new ComplexNumber(resultReal, resultImaginary);
+    }
+
+    @Override
+    public String toString() {
+        return real + " + " + imaginary + "i";
+    }
+}
---
+++ src/Laboratory1/Exercise1/Main.java
@@ -0,0 +1,50 @@
+package Laboratory1.Exercise1;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter real part for first nr");
+        int real1 = scanner.nextInt();
+
+        System.out.println("enter img part for first nr");
+        int img1 = scanner.nextInt();
+
+        System.out.println("enter real part for second nr");
+        int real2 = scanner.nextInt();
+
+        System.out.println("enter img part for second nr");
+        int img2 = scanner.nextInt();
+
+        ComplexNumber complex1 = new ComplexNumber(real1, img1);
+        ComplexNumber complex2 = new ComplexNumber(real2, img2);
+
+        System.out.println("1 = +");
+        System.out.println("2 = -");
+        System.out.println("3 = *");
+        System.out.print("choose (1 or 2 or 3)");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                displayResult("add", complex1.add(complex2));
+                break;
+            case 2:
+                displayResult("sub", complex1.subtract(complex2));
+                break;
+            case 3:
+                displayResult("mul", complex1.multiply(complex2));
+                break;
+            default:
+                System.out.println("invalid choice; choose 1, 2, or 3.");
+        }
+
+        scanner.close();
+    }
+
+    private static void displayResult(String operation, ComplexNumber result) {
+        System.out.println(operation + " result: " + result);
+    }
+}
