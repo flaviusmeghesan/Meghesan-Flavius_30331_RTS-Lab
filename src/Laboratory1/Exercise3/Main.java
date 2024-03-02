23a2b48bd85a2550fcce1d10d33a27012c27932f
rlkk <flv.meghe@gmail.com>
2024-03-02 22:59:37 +0200

    Laboratory 1 ex3 done

---
+++ src/Laboratory1/Exercise3/Main.java
@@ -0,0 +1,25 @@
+package Laboratory1.Exercise3;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int[] randomNumbers = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            randomNumbers[i] = random.nextInt(100);
+        }
+
+        System.out.println("random array");
+        for (int num : randomNumbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n");
+
+        Arrays.sort(randomNumbers);
+
+        System.out.println("sorted array");
+        for (int num : randomNumbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
