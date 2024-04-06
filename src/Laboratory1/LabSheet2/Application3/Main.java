5e29d4d5dc3b1d87cede30ea09a5fdc10ca33701
rlkk <flv.meghe@gmail.com>
2024-04-06 22:49:58 +0300

    app 3 squares fix

--- src/Laboratory1/LabSheet2/Application3/Main.java
+++ src/Laboratory1/LabSheet2/Application3/Main.java
@@ -3,18 +3,15 @@ class Main {
     private static final int noOfThreads = 3;
 
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            runGameIteration();
-        }
-    }
-
-    private static void runGameIteration() {
         Window win = new Window(noOfThreads);
         for (int i = 0; i < noOfThreads; i++) {
             Fir f = new Fir(i, i+2, win);
             f.addObserver(win);
             f.start();
         }
+
     }
+
+
 }
 
